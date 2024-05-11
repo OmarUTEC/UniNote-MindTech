e8ffc2f379c17491d8d4c6404f2303493b893cca
NoeParedes <ananias.paredes@utec.edu.pe>
2024-05-11 01:02:09 -0500

    DarkTheme -advance

--- fronted/src/pages/Login.jsx
+++ fronted/src/pages/Login.jsx
@@ -1,46 +1,49 @@
 import React from "react";
+import useTheme from "../theme";
 
 const Login = () => {
+  const { darkMode } = useTheme();
+
   return (
-    <div className="bg-cach-l2 min-h-screen flex items-center justify-center">
-      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h1 className="text-2xl font-bold text-cach-l4 mb-4">Iniciar sesión</h1>
+    <div className={`min-h-screen flex items-center justify-center ${darkMode ? 'bg-gray-900 text-white' : 'bg-cach-l2 text-black'}`}>
+      <div className={`shadow-md rounded px-8 pt-6 pb-8 mb-4 ${darkMode ? 'bg-gray-800' : 'bg-white'}`}>
+        <h1 className={`text-2xl font-bold mb-4 ${darkMode ? 'text-white' : 'text-black'}`}>Iniciar sesión</h1>
         <form>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+            <label className={`block text-sm font-bold mb-2 ${darkMode ? 'text-white' : 'text-gray-700'}`} htmlFor="username">
               Usuario
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${darkMode ? 'bg-gray-700 text-white' : 'bg-white'}`}
               id="username"
               type="text"
               placeholder="Usuario"
             />
           </div>
           <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            <label className={`block text-sm font-bold mb-2 ${darkMode ? 'text-white' : 'text-gray-700'}`} htmlFor="password">
               Contraseña
             </label>
             <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              className={`shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline ${darkMode ? 'bg-gray-700 text-white' : 'bg-white'}`}
               id="password"
               type="password"
               placeholder="******************"
             />
-            <p className="text-red-500 text-xs italic">Por favor ingrese su contraseña.</p>
+            <p className={`text-red-500 text-xs italic ${darkMode ? 'text-white' : 'text-red-500'}`}>Por favor ingrese su contraseña.</p>
           </div>
           <div className="flex items-center justify-between">
             <button
-              className="bg-cach-l2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${darkMode ? 'bg-gray-600' : ''}`}
               type="button"
             >
               Iniciar sesión
             </button>
             <button
-                className="inline-block align-baseline font-bold text-sm text-cach-l2 hover:text-blue-800"
-                onClick={() => console.log("Haz clic en '¿Olvidaste tu contraseña?'")}
-                >
-                ¿Olvidaste tu contraseña?
+              className={`inline-block align-baseline font-bold text-sm ${darkMode ? 'text-white' : 'text-blue-500'} hover:text-blue-800`}
+              onClick={() => console.log("Haz clic en '¿Olvidaste tu contraseña?'")}
+            >
+              ¿Olvidaste tu contraseña?
             </button>
           </div>
         </form>
--- fronted/src/theme.jsx
+++ fronted/src/theme.jsx
@@ -1,20 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const useTheme = () => {
-  // Estado para controlar el tema (claro u oscuro)
   const [darkMode, setDarkMode] = useState(() => {
-    // Leer el tema seleccionado de localStorage al cargar la página
     const savedTheme = localStorage.getItem('theme');
     return savedTheme === 'dark';
   });
 
-  // Función para alternar entre el modo claro y oscuro
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
 
   useEffect(() => {
-    // Almacenar el tema seleccionado en localStorage
     localStorage.setItem('theme', darkMode ? 'dark' : 'light');
   }, [darkMode]);
 
@@ -23,21 +19,20 @@ const useTheme = () => {
 
 export default useTheme;
 
-// theme.js
-
 const lightTheme = {
-    backgroundColor: 'bg-cach-l2',
-    textColor: 'text-black',
-    navColor: 'bg-cach-l1',
-    navTextColor: 'text-cach-l2'
-  };
-  
-  const darkTheme = {
-    backgroundColor: 'bg-gray-900',
-    textColor: 'text-white',
-    navColor: 'bg-gray-800',
-    navTextColor: 'text-white'
-  };
-  
-  export { lightTheme, darkTheme };
-  
+  backgroundColor: 'bg-cach-l2',
+  textColor: 'text-black',
+  navColor: 'bg-cach-l1',
+  navTextColor: 'text-cach-l2',
+  buttonColor: 'hover:bg-gray-200'
+};
+
+const darkTheme = {
+  backgroundColor: 'bg-gray-900',
+  textColor: 'text-white',
+  navColor: 'bg-gray-800',
+  navTextColor: 'text-white',
+  buttonColor: 'hover:bg-gray-700'
+};
+
+export { lightTheme, darkTheme };
