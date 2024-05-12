b7dedc86a8503bc68f3c69c80b9d62d40ff16f0f
NoeParedes <ananias.paredes@utec.edu.pe>
2024-05-11 20:03:39 -0500

    Loguin password - preview

--- fronted/src/pages/Login.jsx
+++ fronted/src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
               placeholder="Usuario"
             />
           </div>
-          <div className="mb-6">
+          <div className="mb-6 relative">
             <label className={`block text-sm font-bold mb-2 ${darkMode ? 'text-white' : 'text-gray-700'}`} htmlFor="password">
               Contraseña
             </label>
@@ -30,6 +30,21 @@ const Login = () => {
               type="password"
               placeholder="******************"
             />
+            <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className={`h-6 w-6 ${darkMode ? "text-white" : "text-gray-600"}`}
+              >
+                <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                <path
+                  fillRule="evenodd"
+                  d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
             <p className={`text-red-500 text-xs italic ${darkMode ? 'text-white' : 'text-red-500'}`}>Por favor ingrese su contraseña.</p>
           </div>
           <div className="flex items-center justify-between">
