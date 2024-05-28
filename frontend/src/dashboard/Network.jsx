37b2ad9b279a51c60863890aa14e7bf56f5c3ef2
alef7893 <82951714+alef7893@users.noreply.github.com>
2024-05-28 18:46:33 -0500

    upload

--- frontend/src/dashboard/Network.jsx
+++ frontend/src/dashboard/Network.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import useTheme from "../theme";
+import './Network.css';
+
+import profilepicture from '../assets/profilepicture.jpg';
+import followers from '../assets/followers.png';
+import following from '../assets/following.png';
 
 const Network = () => {
+  const { darkMode } = useTheme();
+  const profileName = "Omar Echevarria"
+  const e_mail = "omar.eche@utec.edu.pe";
+  const numFollowers = 43929;
+  const numFollowing = 38;
+  
   return (
-    <div>
-      <h2 className="text-xl font-bold mb-2">Network</h2>
-      <p>Aquí puedes explorar nuestra Network de recursos.</p>
+    <div className={`min-h-screen flex items-center justify-center ${darkMode ? 'bg-gray-900 text-white' : 'bg-cach-l2 text-black'}`}>
+      
+      <div className='container'>
+        <div className={`custom-profile`}>
+          <img src={profilepicture} alt="Upload File" className={`profile-image`} />
+          <div className="profile-details">
+            <div className="profile-info"> <h2>{profileName}</h2> </div>
+            <div className="profile-info"> <h2>{e_mail}</h2> </div>
+            <button className='drop-profile-button'>ELIMINAR PERFIL</button>
+          </div>
+        </div>
+
+        <div className={`custom-profile`}>
+          <button className={`custom-button ${darkMode ? 'bg-gray-800' : 'bg-white'}`}>
+            <img src={followers} alt="Upload File" className={`button-image`} />
+            <div>
+            <h2 className={`button-text ${darkMode ? 'text-white' : 'text-black'}`}>FOLLOWERS</h2>
+            <h2 className={`button-text ${darkMode ? 'text-white' : 'text-black'}`}>{numFollowers}</h2>
+            </div>
+          </button> 
+        
+          <button className={`custom-button ${darkMode ? 'bg-gray-800' : 'bg-white'}`}>
+            <img src={following} alt="Upload File" className={`button-image`} />
+            <div>
+            <h2 className={`button-text ${darkMode ? 'text-white' : 'text-black'}`}>FOLLOWING</h2>
+            <h2 className={`button-text ${darkMode ? 'text-white' : 'text-black'}`}>{numFollowing}</h2>
+            </div>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
