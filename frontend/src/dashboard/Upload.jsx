3fd746554bf235811d35f310a9e19d249b006787
alef7893 <82951714+alef7893@users.noreply.github.com>
2024-05-28 18:42:37 -0500

    upload

--- frontend/src/dashboard/Upload.jsx
+++ frontend/src/dashboard/Upload.jsx
@@ -1,10 +1,124 @@
 import React from 'react';
+import useTheme from "../theme";
+import './Upload.css';
+
+import upload_file from '../assets/upload_file.png';
+import download_file_button from '../assets/download_file_button.png';
+import edit_file_button from '../assets/edit_file_button.png';
+import drop_file_button from '../assets/drop_file_button.png';
+import image_file from '../assets/file_image.png';
 
 const Upload = () => {
+  const { darkMode } = useTheme();
+  const title = 'Matemáticas Discretas 2 - Grafos';
+  const author = "Omar";
+  const uploadedFiles = 1;
+
+  if(uploadedFiles === 0) {
+    return (
+      <div className={`min-h-screen flex items-center justify-center ${darkMode ? 'bg-gray-900 text-white' : 'bg-cach-l2 text-black'}`}>
+        
+        <div className={`not-uploaded-file`}>
+          <p>NO CUENTA CON NINGUN ARCHIVO DISPONIBLE PARA EDITAR</p>
+          <button className={`item upload-file-button ${darkMode ? 'bg-gray-800' : 'bg-white'}`}>
+          <img src={upload_file} alt="Upload File" className={`upload-file-image`}/>
+          <p className={`upload-file-button-text ${darkMode ? 'text-white' : 'text-black'}`}>SUBIR NUEVO ARCHIVO</p>
+          </button>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div>
-      <h2 className="text-xl font-bold mb-2">Upload</h2>
-      <p>Aquí puedes explorar nuestra Upload de recursos.</p>
+    <div className={`min-h-screen flex items-center justify-center ${darkMode ? 'bg-gray-900 text-white' : 'bg-cach-l2 text-black'}`}>
+
+    <div className={`item-controller`}>
+      
+      <button className={`item upload-file-button ${darkMode ? 'bg-gray-800' : 'bg-white'}`}>
+        <img src={upload_file} alt="Upload File" className={`upload-file-image`}/>
+        <p className={`upload-file-button-text ${darkMode ? 'text-white' : 'text-black'}`}>SUBIR NUEVO ARCHIVO</p>
+      </button>
+      
+      <div className={`item uploaded-file-item`}>
+        <img src={image_file} alt="File" className='view-file'/>
+        <div className={`file-description`}>
+          <p>{title}</p>
+          <p>Autor: {author}</p>
+        </div>
+        <div className={`options-bar`}>
+          <button> <img src={download_file_button} alt="Option" className='image-option' /> </button>
+          <button> <img src={edit_file_button} alt="Option" className='image-option' /> </button>
+          <button> <img src={drop_file_button} alt="Option" className='image-option' /> </button>
+        </div>
+      </div>
+
+      <div className={`item uploaded-file-item`}>
+        <img src={image_file} alt="File" className='view-file'/>
+        <div className={`file-description`}>
+          <p>{title}</p>
+          <p>Autor: {author}</p>
+        </div>
+        <div className={`options-bar`}>
+          <button> <img src={download_file_button} alt="Option" className='image-option' /> </button>
+          <button> <img src={edit_file_button} alt="Option" className='image-option' /> </button>
+          <button> <img src={drop_file_button} alt="Option" className='image-option' /> </button>
+        </div>
+      </div>
+
+      <div className={`item uploaded-file-item`}>
+        <img src={image_file} alt="File" className='view-file'/>
+        <div className={`file-description`}>
+          <p>{title}</p>
+          <p>Autor: {author}</p>
+        </div>
+        <div className={`options-bar`}>
+          <button> <img src={download_file_button} alt="Option" className='image-option' /> </button>
+          <button> <img src={edit_file_button} alt="Option" className='image-option' /> </button>
+          <button> <img src={drop_file_button} alt="Option" className='image-option' /> </button>
+        </div>
+      </div>
+
+      <div className={`item uploaded-file-item`}>
+        <img src={image_file} alt="File" className='view-file'/>
+        <div className={`file-description`}>
+          <p>{title}</p>
+          <p>Autor: {author}</p>
+        </div>
+        <div className={`options-bar`}>
+          <button> <img src={download_file_button} alt="Option" className='image-option' /> </button>
+          <button> <img src={edit_file_button} alt="Option" className='image-option' /> </button>
+          <button> <img src={drop_file_button} alt="Option" className='image-option' /> </button>
+        </div>
+      </div>
+
+
+      <div className={`item uploaded-file-item`}>
+        <img src={image_file} alt="File" className='view-file'/>
+        <div className={`file-description`}>
+          <p>{title}</p>
+          <p>Autor: {author}</p>
+        </div>
+        <div className={`options-bar`}>
+          <button> <img src={download_file_button} alt="Option" className='image-option' /> </button>
+          <button> <img src={edit_file_button} alt="Option" className='image-option' /> </button>
+          <button> <img src={drop_file_button} alt="Option" className='image-option' /> </button>
+        </div>
+      </div>
+
+
+      <div className={`item uploaded-file-item`}>
+        <img src={image_file} alt="File" className='view-file'/>
+        <div className={`file-description`}>
+          <p>{title}</p>
+          <p>Autor: {author}</p>
+        </div>
+        <div className={`options-bar`}>
+          <button> <img src={download_file_button} alt="Option" className='image-option' /> </button>
+          <button> <img src={edit_file_button} alt="Option" className='image-option' /> </button>
+          <button> <img src={drop_file_button} alt="Option" className='image-option' /> </button>
+        </div>
+      </div>
+
+    </div>
     </div>
   );
 };
