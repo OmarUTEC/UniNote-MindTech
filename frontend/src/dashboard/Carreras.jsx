781cede40fa33dd3450db4858555907fa2d03a36
NoeParedes <ananias.paredes@utec.edu.pe>
2024-06-02 05:51:55 -0500

    Update Changes

--- frontend/src/dashboard/Carreras.jsx
+++ frontend/src/dashboard/Carreras.jsx
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import useTheme from "../theme";
-import { Link } from "react-router-dom";
-
-// Importar las imágenes
-import ComputerScience from "../assets/userstudent.jpeg";
-import electronica from "../assets/userstudent.jpeg";
-import mecatronica from "../assets/userstudent.jpeg";
-import informatica from "../assets/userstudent.jpeg";
-import telecomunicaciones from "../assets/userstudent.jpeg";
-import malware from "../assets/userstudent.jpeg";
-
-const Carreras = () => {
-  const { darkMode } = useTheme();
-
-  const carreras = [
-    { id: 1, name: "Computer Science", image: ComputerScience, path: "cs" },
-    { id: 2, name: "Ingeniería en Electrónica", image: electronica, path: "electronica" },
-    { id: 3, name: "Ingeniería en Mecatrónica", image: mecatronica, path: "mecatronica" },
-    { id: 4, name: "Ingeniería en Informática", image: informatica, path: "informatica" },
-    { id: 5, name: "Ingeniería en Telecomunicaciones", image: telecomunicaciones, path: "telecomunicaciones" },
-    { id: 6, name: "Seguridad Informática", image: malware, path: "seguridadinformatica" },
-  ];
-
-  return (
-    <div className={`min-h-screen flex items-center justify-center ${darkMode ? 'bg-gray-900 text-white' : 'bg-gray-100 text-black'}`}>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {carreras.map((carrera) => (
-          <Link
-            key={carrera.id}
-            to={`/carreras/${carrera.path}`}
-            className={`relative block rounded-lg overflow-hidden shadow-lg transform transition-transform hover:scale-105 ${darkMode ? 'bg-gray-800' : 'bg-white'}`}
-          >
-            <img src={carrera.image} alt={carrera.name} className="w-4/6 h-70 object-cover mx-auto" />
-            <div className={`absolute bottom-0 left-0 w-full p-2 text-center ${darkMode ? 'bg-gray-800 bg-opacity-75 text-white' : 'bg-white bg-opacity-75 text-black'}`}>
-              <p className="font-semibold">{carrera.name}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-export default Carreras;
-=======
 import React from "react";
 import useTheme from "../theme";
 import { Link } from "react-router-dom";
@@ -90,4 +43,3 @@ const Carreras = () => {
 }
 
 export default Carreras;
->>>>>>> fdda2c0 (update images)
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,15 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import useTheme from "../theme"; 
-import { darkTheme, lightTheme } from "../theme"; 
-
+import { Link } from 'react-router-dom';
+import useTheme from '../theme';
+import { darkTheme, lightTheme } from '../theme';
 import logo from '../assets/logo.png';
 import userstudent from '../assets/userstudent.jpeg';
+import meta1 from '../assets/meta1.png';
+import meta2 from '../assets/meta2.png';
+import meta3 from '../assets/meta3.png';
 
 const LandingPage = () => {
-  const { darkMode, toggleDarkMode } = useTheme(); 
+  const { darkMode, toggleDarkMode } = useTheme();
   const theme = darkMode ? darkTheme : lightTheme;
 
+  const metas = [
+    {
+      title: 'Meta 1',
+      description: 'Descripción de la meta 1.',
+      image: meta1,
+    },
+    {
+      title: 'Meta 2',
+      description: 'Descripción de la meta 2.',
+      image: meta2,
+    },
+    {
+      title: 'Meta 3',
+      description: 'Descripción de la meta 3.',
+      image: meta3,
+    },
+  ];
+  const handleMetasClick = () => {
+    const metasSection = document.getElementById('metas');
+    metasSection.scrollIntoView({ behavior: 'smooth' });
+  };
   return (
     <div className={`min-h-screen ${theme.backgroundColor}`}>
       
@@ -19,18 +41,35 @@ const LandingPage = () => {
           
           <div className="flex items-center">
             <img src={logo} alt="UniNote" className="h-12" />
-            <span className={`ml-2 text-lg font-bold ${theme.navTextColor}`}> UniNote </span>
+            <span className={`ml-2 text-lg font-bold ${theme.navTextColor}`}>UniNote</span>
           </div>
 
           <nav className="flex items-center">
-            <Link to="/" className={`ml-2 mr-4 text-lg font-bold ${theme.navTextColor}`}> Inicio </Link>
-            <Link to="/metas" className={`ml-2 mr-4 text-lg font-bold ${theme.navTextColor}`}> Metas </Link>
-            <Link to="/login" className={`ml-2 text-lg font-bold ${theme.navTextColor} `}> Iniciar sesión </Link>
-            
-            <button onClick={toggleDarkMode} className={`ml-2 px-4 py-2 rounded-md ${darkMode ? 'text-white bg-transparent' : 'text-blue-500 ' }`}>
+            <Link to="/" className={`ml-2 mr-4 text-lg font-bold ${theme.navTextColor}`}>
+              Inicio
+            </Link>
+            <a
+              href="#metas"
+              className={`ml-2 mr-4 text-lg font-bold ${theme.navTextColor}`}
+              onClick={handleMetasClick}
+            >
+              Metas
+            </a>
+            <Link to="/login" className={`ml-2 text-lg font-bold ${theme.navTextColor}`}>
+              Iniciar sesión
+            </Link>
+            <button
+              onClick={toggleDarkMode}
+              className={`ml-2 px-4 py-2 rounded-md ${
+                darkMode ? 'text-white bg-transparent' : 'text-blue-500 '
+              }`}
+            >
               {darkMode ? (
                 <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                  <path fill="currentColor" d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/>
+                  <path
+                    fill="currentColor"
+                    d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"
+                  />
                 </svg>
               ) : (
                 <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -41,21 +80,35 @@ const LandingPage = () => {
           </nav>
         </div>
       </header>
-
-      <main className="container mx-auto py-8 px-8 flex items-center">
-        <div className="w-1/2 pr-4">
-          <h1 className={`text-5xl font-bold mb-4 ${theme.textColor}`}>"COMPARTIR PARA APRENDER"</h1>
-          <h2 className={`text-3xl font-semibold mb-2 ${theme.textColor}`}>Te ofrecemos publicar y descubrir papers y apuntes de guía académica</h2>
-          <p className={`text-3xl mb-4 ${theme.textColor}`}>Una plataforma dirigida a los estudiantes que quieren aprender y compartir sus aprendizajes</p>
-        </div>
-        <div className="w-1/2 relative">
-          <div className={`absolute top-0 left-0 w-full h-full ${darkMode ? 'bg-gradient-to-r from-gray-900' : 'bg-gradient-to-r from-cach-l2'}`}></div>
-          <img src={userstudent} alt="Usuario estudiante" className="rounded-lg shadow-md" />
-        </div>
-      </main>
-
+    <main className="container mx-auto py-8 px-8 flex flex-col md:flex-row items-center">
+    <div className="w-full md:w-1/2 pr-4 mb-8 md:mb-0">
+      <h1 className={`text-5xl font-bold mb-4 ${theme.textColor}`}>"COMPARTIR PARA APRENDER"</h1>
+      <h2 className={`text-3xl font-semibold mb-2 ${theme.textColor}`}>Te ofrecemos publicar y descubrir papers y apuntes de guía académica</h2>
+      <p className={`text-3xl mb-4 ${theme.textColor}`}>Una plataforma dirigida a los estudiantes que quieren aprender y compartir sus aprendizajes</p>
     </div>
-  );
+    <div className="w-full md:w-1/2 relative">
+      <div className={`absolute top-0 left-0 w-full h-full ${darkMode ? 'bg-gradient-to-r from-gray-900' : 'bg-gradient-to-r from-cach-l2'}`}></div>
+      <img src={userstudent} alt="Usuario estudiante" className="rounded-lg shadow-md w-full" />
+    </div>
+  </main>
+  <section id="metas" className="container mx-auto py-8 px-8">
+    <h2 className={`text-4xl font-bold mb-8 ${theme.textColor}`}>Nuestras Metas</h2>
+    <div className="flex flex-wrap justify-center">
+      {metas.map((meta, index) => (
+        <div key={index} className={`w-full md:w-1/4 p-4`}>
+          <div className={`rounded-lg shadow-md p-4 ${darkMode ? 'bg-black-800' : 'bg-gray-100'}`}>
+            <div className="flex items-center mb-2">
+              <h3 className={`text-lg font-bold ${theme.textColor}`}>{meta.title}</h3>
+            </div>
+            <img src={meta.image} alt={meta.title} className="mb-2 w-full h-32 object-cover" />
+            <p className={`text-sm ${theme.textColor}`}>{meta.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  </section>
+</div>
+);
 };
 
 export default LandingPage;
