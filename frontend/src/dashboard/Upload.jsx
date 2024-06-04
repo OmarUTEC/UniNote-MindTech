f999274b9f420ea035e7e1c57aad1cbae2713104
NoeParedes <ananias.paredes@utec.edu.pe>
2024-06-04 17:41:17 -0500

    Upload

--- frontend/src/dashboard/Upload.jsx
+++ frontend/src/dashboard/Upload.jsx
@@ -1,19 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import useTheme from "../theme";
+import UploadIcon from './UploadIcon';
 import Card from './cards';
-import UploadIcon from './UploadIcon'; // Import the SVG component
 
-const Upload = ({ handleUploadFileClick }) => {
+const Upload = ({ userId, handleUploadFileClick }) => {
   const { darkMode } = useTheme();
-  const title = 'Matemáticas Discretas 2 - Grafos';
-  const author = "Omar";
-  const uploadedFiles = 1;
-
   const [documents, setDocuments] = useState([]);
+  const [uploadedFiles, setUploadedFiles] = useState(0);
+  const hasFetched = useRef(false);
+
   useEffect(() => {
     const fetchDocuments = async () => {
       try {
-        const response = await fetch('http://127.0.0.1:5000/documents', {
+        const response = await fetch(`http://127.0.0.1:5000/documents/user/${userId}`, {
           method: 'GET',
           headers: { 'Content-Type': 'application/json' }
         });
@@ -22,12 +21,16 @@ const Upload = ({ handleUploadFileClick }) => {
         }
         const data = await response.json();
         setDocuments(data);
+        setUploadedFiles(data.length);
       } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
       }
     };
-    fetchDocuments();
-  }, []);
+    if (!hasFetched.current) {
+      fetchDocuments();
+      hasFetched.current = true; // marca como verdadero después de la primera llamada
+    }
+  }, [userId]);
 
   if (uploadedFiles === 0) {
     return (
@@ -38,10 +41,10 @@ const Upload = ({ handleUploadFileClick }) => {
             onClick={handleUploadFileClick}
             className={`rounded-lg border-2 border-black ${darkMode ? 'bg-gray-800' : 'bg-white'} shadow-md p-4 flex flex-col items-center w-48 h-48`}
           >
-            <div className="w-24 h-24"> {/* Ajustar el tamaño del contenedor */}
+            <div className="w-24 h-24">
               <UploadIcon />
             </div>
-            <p className="text-base mt-4">SUBIR NUEVO ARCHIVO</p>
+            <p className="text-base mt-4">SUBIR NUEVO ARCHIVO {userId}</p>
           </button>
         </div>
       </div>
@@ -51,16 +54,22 @@ const Upload = ({ handleUploadFileClick }) => {
   return (
     <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 p-12 overflow-y-auto ${darkMode ? 'bg-gray-900 text-white' : 'bg-cach-l2 text-black'}`}>
       <button onClick={handleUploadFileClick} className={`flex flex-col items-center w-full h-full rounded-lg border-2 border-black ${darkMode ? 'bg-gray-800' : 'bg-white'} shadow-md p-4`}>
-        <div className="w-24 h-24"> {/* Ajustar el tamaño del contenedor */}
-          <UploadIcon />
+        <div className="flex items-center justify-center w-full h-full">
+          <UploadIcon className="w-3/5 h-3/5 object-cover rounded-lg" />
         </div>
-        <p className="text-base font-bold mt-4">SUBIR NUEVO ARCHIVO</p>
+        <p className="text-base font-bold mt-4">
+          SUBIR NUEVO ARCHIVO {userId}
+        </p>
       </button>
-  
+
       {documents.map((document, index) => (
-        <Card key={index} title={document.titulo} author={document.usuario_id} darkMode={darkMode} />
+        <Card
+          key={index}
+          title={document.titulo}
+          author={document.usuario_id}
+          darkMode={darkMode}
+        />
       ))}
-  
     </div>
   );
 };
